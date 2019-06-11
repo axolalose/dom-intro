9efa3adca041f8ba4c2e477ee7a1dbfcb030a4f0
axolalose <axolalose@gmail.com>
2019-06-11 11:29:43 +0200

    changes

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -17,14 +17,15 @@ function calculateBill() {
         }
     }
     if (billTotal3 >= 20) {
-        billTotalElement.classList.add("warning")
-        billTotalElement.classList.remove("danger")
+        billTotalElement.classList.add("warning");
+        billTotalElement.classList.remove("danger");
 
     }
    if (billTotal3 >= 30) {
-        billTotalElement.classList.remove("warning")
-        billTotalElement.classList.add("danger")
-    }else {
+        billTotalElement.classList.remove("warning");
+        billTotalElement.classList.add("danger");
+    }
+    if(billTotal3 < 20)  {
         billTotalElement.classList.remove("danger");
         billTotalElement.classList.remove("warning");
     }
